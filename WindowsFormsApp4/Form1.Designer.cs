ef42b0689a1cff26864785eace647619c65e47e1
Phạm Quang Hoà <20520995@gm.uit.edu.vn>
2022-06-09 22:18:15 +0700

    a

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -398,7 +398,7 @@
             this.Name = "Form1";
             this.Opacity = 0.98D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "HBBookShop";
+            this.Text = "HBBookSho";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabPage3.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
